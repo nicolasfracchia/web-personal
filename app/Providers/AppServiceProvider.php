45916de2fc0154b90a7e30656f462087d1c1ec29
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-05-19 17:59:14 -0600

    Added blade directive in AppServiceProvider.php to automatically format dates in any template using @

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /* Format date to show only the name of the month and the year */
+        Blade::directive('formatDateFY', function ($date) {
+            return "<?php echo \Carbon\Carbon::parse($date)->format('F Y'); ?>";
+        });
     }
 }
