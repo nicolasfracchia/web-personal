4e17d615310357fcfb0b1df123cc1010bc06c4f1
nicolasfracchia <nfracchia@hotmail.com>
2024-07-07 10:40:04 -0600

    main section moved into a component for better index readability

---
+++ resources/views/components/main-header.blade.php
@@ -0,0 +1,28 @@
+<main>
+    <header>
+        <figure>
+            <img src="{{asset('profile.jpg')}}" alt="Nicolas Fracchia - profile photo" />
+        </figure>
+        <h1>NICOLAS FRACCHIA</h1>
+        <h2>Full-Stack Software Developer</h2>
+    </header>
+    <address>
+        <h3> @include('icons/caret-svg') Contact information</h3>
+        <a href='https://maps.app.goo.gl/rHaMfQvnjFhhTt11A' target="_blank">
+            @include('icons/location-svg') 
+            Calgary, Alberta, canada.
+        </a>
+        <a href='tel:+15874372920'>
+            @include('icons/call-svg') 
+            +1 (587) 437 - 2920
+        </a>
+        <a href='mailto:dev.nfracchia@gmail.com' target="_blank">
+            @include('icons/mail-svg') 
+            dev.nfracchia@gmail.com
+        </a>
+        <a href='https://www.linkedin.com/in/nicolasfracchia/' target="_blank">
+            @include('icons/linkedin-svg') 
+            /nicolasfracchia
+        </a>
+    </address>
+</main>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,35 +11,7 @@
 </head>
 <body>
     
-    <main>
-        <header>
-            <figure>
-                <img src="{{asset('profile.jpg')}}" alt="Nicolas Fracchia - profile photo" />
-            </figure>
-            <h1>NICOLAS FRACCHIA</h1>
-            <h2>Full-Stack Software Developer</h2>
-        </header>
-        <address>
-            <h3> @include('icons/caret-svg') Contact information</h3>
-            <a href='https://maps.app.goo.gl/rHaMfQvnjFhhTt11A' target="_blank">
-                @include('icons/location-svg') 
-                Calgary, Alberta, canada.
-            </a>
-            <a href='tel:+15874372920'>
-                @include('icons/call-svg') 
-                +1 (587) 437 - 2920
-            </a>
-            <a href='mailto:dev.nfracchia@gmail.com' target="_blank">
-                @include('icons/mail-svg') 
-                dev.nfracchia@gmail.com
-            </a>
-            <a href='https://www.linkedin.com/in/nicolasfracchia/' target="_blank">
-                @include('icons/linkedin-svg') 
-                /nicolasfracchia
-            </a>
-        </address>
-    </main>
-
+    @include('components.main-header')
     
     @if (isset($about) && !empty($about))
         @include('components.section-container', ['title' => 'About Me','content' => $about])
