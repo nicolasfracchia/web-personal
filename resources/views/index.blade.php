132409fbc791de1032ec2dea3b0cd414d9d60dfd
nicolasfracchia <nfracchia@hotmail.com>
2024-07-07 10:35:41 -0600

    SEO OPTIMIZATION: title and meta description

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NICOLAS FRACCHIA | Full-Stack Software Developer</title>
+    <title>NICOLAS FRACCHIA | Full-Stack Software Developer | Calgary</title>
+    <meta name="description" content="Full-Stack Developer with 10+ years of experience | Custom E-commerce & Reports Developer | JavaScript, PHP, MySQL, Back-End Optimization.">
     <link rel="shortcut icon" href="favicon.ico" />
     <link rel="stylesheet" href="{{asset('css/general.css')}}">
 </head>
